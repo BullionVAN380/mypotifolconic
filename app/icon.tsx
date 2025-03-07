d6998b65eebaa1195cf2ee26b37024ee5060a9bd
BullionVAN380 <josphatbaraka3@gmail.com>
2025-03-08 02:00:03 +0300

    favicon change

--- app/icon.tsx
+++ app/icon.tsx
@@ -4,7 +4,7 @@ export const size ={
     width : 32,
     height :32,
 };
-export const contentType ="image/png";
+export const contentType ="image/jpg";
 export default function Icon(){
     return new ImageResponse (
     (
